254010490a9be0e849c3be13479034944cac56f9
Nicolas-B06 <nicolas.bernard@nextsourcia.com>
2023-04-13 16:18:13 +0200

    Feat : create header component with logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,170 @@
-import { Link } from "react-router-dom";
-import "./Header.css"
-import { useSelector } from 'react-redux';
+import * as React from 'react';
+import { Link as RouterLink } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import setLoggedUser from '../store/reducers/auth';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
   const loggedUser = useSelector(state => state.auth.loggedUser);
-  console.log(loggedUser)
+
+  let pages = { 'Home': '/' };
+  let settings = { 'se connecter': '/logIn', "s'enregistrer": "/signIn" };
+
+  if (loggedUser) {
+    pages = { 'Home': '/', 'User List': '/userList' };
+    settings = { 'Profile': '/Profile', 'se déconnecter': '/logIn' };
+  }
+
+
+  const dispatch = useDispatch();
+
+  const disconnect = () => {
+    localStorage.removeItem('TOKEN');
+    dispatch(setLoggedUser(null))
+  };
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <nav className="Header">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/userList">User list</Link></li>
-        {!loggedUser && <li><Link to="/logIn">Se connecter</Link></li>}
-        {!loggedUser && <li><Link to="/signIn">S'inscrire</Link></li>}
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Ynov Workplace
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {Object.entries(pages).map(([key, link]) => (
+                <MenuItem key={key} onClick={handleCloseNavMenu} component={RouterLink} to={link}>
+                  <Typography textAlign="center">{key}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {Object.entries(pages).map(([key, link]) => (
+              <Button
+                key={key}
+                component={RouterLink}
+                to={link}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {key}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <AccountCircleIcon fontSize="large" sx={{ color: "white" }} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {Object.entries(settings).map(([key, link]) => (
+                key === 'se déconnecter' ? (
+                  <MenuItem key={key} component={RouterLink} to={link} onClick={() => {
+                    handleCloseUserMenu();
+                    disconnect();
+                  }}>
+                    <Typography textAlign="center">{key}</Typography>
+                  </MenuItem>
+                ) : (
+                  <MenuItem key={key} component={RouterLink} to={link} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{key}</Typography>
+                  </MenuItem>)
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
 export default Header;
