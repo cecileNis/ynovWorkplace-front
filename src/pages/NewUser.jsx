2419198d08e2aa62a804c4ad50f158dd0613dd38
Stephane0713 <stephaned2205@gmail.com>
2023-04-16 10:54:44 +0200

    #15 done; changed group card

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,40 +1,56 @@
-import { Button, TextField } from '@mui/material';
+import { Button, TextField } from "@mui/material";
+import { API_URL } from "../conf/api.conf";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 
 function NewUser() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [nickname, setNickname] = useState("");
-  const url = "http://82.65.6.187:8002/api/users"
+  const url = API_URL + "/api/users";
 
   const onCreate = async () => {
     try {
       let payload = { email, plainPassword: password, nickname };
       console.log(payload);
-      let user = await axios.post(
-        url,
-        payload,
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
+      let user = await axios.post(url, payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       console.log(user);
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <>
-      <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <TextField id="outlined-basic" label="Nickname" variant="outlined" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-      <TextField id="outlined-basic" label="Password" variant="outlined" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Nickname"
+        variant="outlined"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Button onClick={onCreate}>S'inscrire</Button>
     </>
-
   );
 }
 
