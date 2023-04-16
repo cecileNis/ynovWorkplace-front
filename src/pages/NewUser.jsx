8c0b895e139ef97f097fa96055a8bb69181aaded
Nicolas-B06 <bernardnicolas06600@gmail.com>
2023-04-16 17:33:05 +0200

    Feat : page Error 404 + login / register design correction

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -4,11 +4,25 @@ import { API_URL } from "../conf/api.conf";
 import React, { useState } from "react";
 import axios from "axios";
 
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 function NewUser() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [nickname, setNickname] = useState("");
   const url = API_URL + "/api/users";
+
+  const theme = createTheme();
 
   const onCreate = async () => {
     try {
@@ -27,7 +41,7 @@ function NewUser() {
 
   return (
     <>
-      <TextField
+      {/* <TextField
         id="outlined-basic"
         label="Email"
         variant="outlined"
@@ -49,7 +63,82 @@ function NewUser() {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button onClick={onCreate}>S'inscrire</Button>
+      <Button onClick={onCreate}>S'inscrire</Button> */}
+
+      <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Crééer un compte
+          </Typography>
+          <Box component="form" noValidate onSubmit={onCreate} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Pseudo"
+                  autoFocus
+                  onChange={(e) => setNickname(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Mot de passe"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              S'inscrire
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/logIn" variant="body2">
+                 vous possédez déjà un compte ? Connectez-vous
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
     </>
   );
 }
