2a5a1d5cb83008b9480420d518893ae42d93480b
Stephane0713 <stephaned2205@gmail.com>
2023-05-29 10:38:08 +0200

    #32 done with login logout part

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(index);
 
 const server = http.createServer(app);
 const io = socketIo(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 });
 
 let interval;
@@ -21,52 +21,54 @@ let users = [];
 let messages = [];
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
+    console.log("New client connected");
 
-  console.log(users);
-
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-
-  socket.on("user login", (user) => {
-    if (!users.find(({ id }) => id === user.id)) users.push({ ...user, socketId: socket.id });
     console.log(users);
-    io.emit("new login", users);
-  });
 
-  socket.on("user logout", (user) => {
-    users = users.filter(({ id }) => user.id !== id);
-    console.log(users);
-    io.emit("new login", users);
-  });
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
 
-  socket.on("loggedUser init", () => {
-    console.log("loggedUser init");
-    io.emit("new login", users);
-  });
+    socket.on("user login", (user) => {
+        if (!users.find(({id}) => id === user.id)) users.push({...user, socketId: socket.id});
+        console.log(users);
+        io.emit("new login", users);
+    });
 
-  socket.on("messages init", (data) => {
-    console.log("messages init");
-    messages = data;
-  });
+    socket.on("user logout", (user) => {
+        users = users.filter(({id}) => user.id !== id);
+        console.log(users);
+        io.emit("new login", users);
+    });
 
-  socket.on("message add", (message) => {
-    messages = [...messages, message];
-    console.log(`new message : ${message}`);
-    io.emit("new message", messages);
-  });
+    socket.on("loggedUser init", () => {
+        console.log("loggedUser init");
+        io.emit("new login", users);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnect");
-    clearInterval(interval);
-  });
+    socket.on("messages init", (data) => {
+        console.log("messages init");
+        messages = data;
+    });
+
+    socket.on("message add", (message) => {
+        messages = [...messages, message];
+        console.log(`new message : ${message}`);
+        io.emit("new message", messages);
+    });
+
+    socket.on("disconnect", (data) => {
+        console.log("Client disconnect");
+        users = users.filter(user => socket.id !== user.socketId)
+        console.log(users)
+        clearInterval(interval);
+    });
 });
 
 const getApiAndEmit = (socket) => {
-  const response = new Date();
-  socket.emit("FromAPI", response);
+    const response = new Date();
+    socket.emit("FromAPI", response);
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- src/index.js
+++ src/index.js
@@ -3,59 +3,64 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
+import {BrowserRouter} from "react-router-dom";
 import axios from "axios";
-import { setUsers } from "./store/reducers/user";
-import { setLoggedUser } from "./store/reducers/auth";
-import { API_URL, INFO_ID } from "./conf/api.conf";
-import { setGroups } from "./store/reducers/group";
-import { setLoading } from "./store/reducers/loading";
+import {setUsers} from "./store/reducers/user";
+import {setLoggedUser} from "./store/reducers/auth";
+import {API_URL, INFO_ID} from "./conf/api.conf";
+import {setGroups} from "./store/reducers/group";
+import {setLoading} from "./store/reducers/loading";
+import socketIOClient from "socket.io-client";
 
 const url = API_URL;
+const ENDPOINT = "http://127.0.0.1:4001";
+const socket = socketIOClient(ENDPOINT);
 
 async function retrieveUsers() {
-  try {
-    let response = await axios.get(`${url}/api/users`);
-    let users = response.data["hydra:member"];
-    store.dispatch(setUsers(users));
-  } catch (e) {
-    store.dispatch(setUsers([]));
-  }
+    try {
+        let response = await axios.get(`${url}/api/users`);
+        let users = response.data["hydra:member"];
+        store.dispatch(setUsers(users));
+    } catch (e) {
+        store.dispatch(setUsers([]));
+    }
 }
 
 async function retrieveLoggedUser() {
-  try {
-    let loggedUser = await axios.get(`${url}/api/users/${INFO_ID}/info`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem("TOKEN")}` },
-    });
-    let user = loggedUser.data;
-    store.dispatch(setLoggedUser(user));
-  } catch (e) {}
+    try {
+        let loggedUser = await axios.get(`${url}/api/users/${INFO_ID}/info`, {
+            headers: {Authorization: `Bearer ${localStorage.getItem("TOKEN")}`},
+        });
+        let user = loggedUser.data;
+        store.dispatch(setLoggedUser(user));
+        socket.emit("user login", {id: user.id, username: user.nickname});
+    } catch (e) {
+    }
 }
 
 async function retrieveGroups() {
-  try {
-    let response = await axios.get(`${url}/api/groups`);
-    let groups = response.data["hydra:member"];
-    store.dispatch(setGroups(groups));
-  } catch (e) {
-    store.dispatch(setGroups([]));
-  }
+    try {
+        let response = await axios.get(`${url}/api/groups`);
+        let groups = response.data["hydra:member"];
+        store.dispatch(setGroups(groups));
+    } catch (e) {
+        store.dispatch(setGroups([]));
+    }
 }
 
 Promise.all([retrieveLoggedUser(), retrieveUsers(), retrieveGroups()]).finally(() => store.dispatch(setLoading(false)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
